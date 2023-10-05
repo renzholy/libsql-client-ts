80f3dac8212a68412d52315c92fff4dd6c5e7606
Aliez <renzholy@gmail.com>
2023-10-05 16:52:30 +0800

    0.3.6-pre.5

--- src/http.ts
+++ src/http.ts
@@ -5,7 +5,7 @@ import {
   Stream,
   openHttp,
   HttpClient as HranaHttpClient,
-} from "@libsql/hrana-client";
+} from "@libsql/hrana-client/lib-esm";
 import type { Config, Client } from "./api.js";
 import type { InStatement, ResultSet, Transaction, IntMode } from "./api.js";
 import { TransactionMode, LibsqlError } from "./api.js";
